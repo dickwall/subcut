19c29f0f4bc90874111db861f97151ea2039ee39
galarragas <galarragas@gmail.com>
2014-03-11 10:29:44 +0000

    Added conversion methods to ConfigProperty trait for direct usage of configuration if necessary

--- src/main/scala/com/escalatesoft/subcut/inject/config/ConfigProperty.scala
+++ src/main/scala/com/escalatesoft/subcut/inject/config/ConfigProperty.scala
@@ -3,10 +3,16 @@ package com.escalatesoft.subcut.inject.config
 sealed trait ConfigProperty {
   def name: String
   def value: String
+  
+  def valueAs[T](implicit converter: ConfigProperty => T) : Option[T]
 }
 
-case class Defined(name: String, value: String) extends ConfigProperty
+case class Defined(name: String, value: String) extends ConfigProperty {
+  def valueAs[T](implicit converter: ConfigProperty => T) : Option[T] = Some( converter(this) )
+}
 
 case class Undefined(name: String) extends ConfigProperty {
   def value = throw new IllegalArgumentException(s"Undefined value for property $name")
+
+  def valueAs[T](implicit converter: ConfigProperty => T) : Option[T] = None
 }
